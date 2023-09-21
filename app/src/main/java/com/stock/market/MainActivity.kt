7a34e095f16315a76f55136016fd2671a0d9b091
Shyak Das <shayak.das007@gmail.com>
2023-09-22 00:23:09 +0530

    [Implementation]Package creation of data domain and presentation layer

--- app/src/main/java/com/stock/market/MainActivity.kt
+++ app/src/main/java/com/stock/market/MainActivity.kt
@@ -3,12 +3,24 @@ package com.stock.market
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.stock.market.ui.theme.StockMarketAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            StockMarketAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
 
+                }
+            }
         }
     }
 }
