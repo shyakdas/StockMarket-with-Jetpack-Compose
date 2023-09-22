a8b130db6f27b5efed742d065f4c3e027f1d59c2
Shyak Das <shayak.das007@gmail.com>
2023-09-22 19:57:27 +0530

    [Implementation]Fix the compile issue

--- app/src/main/java/com/stock/market/data/local/StockDatabase.kt
+++ app/src/main/java/com/stock/market/data/local/StockDatabase.kt
@@ -1,9 +1,10 @@
 package com.stock.market.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [CompanyListingEntity::class], version = 1)
-abstract class StockDatabase {
+abstract class StockDatabase : RoomDatabase() {
 
     abstract val dao: StockDao
 }
